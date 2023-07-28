d25f31f1de0b271b1dfcc95900ae6aba3e950435
Holden <94766608+HBothun@users.noreply.github.com>
2023-07-28 15:48:26 -0500

    Update and rename StartMenue.cs to StartMenu.cs
    lol spelling

--- StartMenu.cs
+++ StartMenu.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class StartMenue : MonoBehaviour
+public class StartMenu : MonoBehaviour
 {
     public Button start;
 
